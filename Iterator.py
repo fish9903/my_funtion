5452cebabdc538da61ea5559818a615a85aa6765
fish9903 <fishcopy6702@gmail.com>
2020-12-16 11:53:33 +0900

    Iterator example.py

---
+++ Iterator.py
@@ -0,0 +1,21 @@
+class TimeIterator:
+    def __init__(self, start, stop):
+        self.stop = stop
+        self.start = start
+
+    def __getitem__(self, item):
+        if item < self.stop - self.start:
+            hour = (self.start + item) // 3600 % 24
+            min = (self.start % 3600 + item) // 60 % 60
+            sec = (self.start + item) % 60
+            return '{0:02d}:{1:02d}:{2:02d}'.format(hour, min, sec)
+        else:
+            raise StopIteration
+
+
+start, stop, index = map(int, input().split())
+
+for i in TimeIterator(start, stop):
+    print(i)
+
+print('\n', TimeIterator(start, stop)[index], sep='')
